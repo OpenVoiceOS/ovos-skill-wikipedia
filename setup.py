2fad385fd6cebe088f1f367e54c40cb1ef725bd2
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-08-22 03:20:55 +0100

    solver plugin included (#17)

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ SKILL_PKG = SKILL_NAME.lower().replace('-', '_')
 PLUGIN_ENTRY_POINT = f'{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}={SKILL_PKG}:{SKILL_CLAZZ}'
 # skill_id=package_name:SkillClass
 
+SOLVER_ENTRY_POINT = f'ovos-solver-plugin-wikipedia={SKILL_PKG}:WikipediaSolver'
+
 
 def get_requirements(requirements_filename: str):
     requirements_file = path.join(path.abspath(path.dirname(__file__)),
@@ -67,5 +69,6 @@ setup(
     include_package_data=True,
     install_requires=get_requirements("requirements.txt"),
     keywords='ovos skill plugin',
-    entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
+    entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT,
+                  'neon.plugin.solver': SOLVER_ENTRY_POINT}
 )
