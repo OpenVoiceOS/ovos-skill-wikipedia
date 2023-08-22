0b2093374387a5c72fd74dfb6f76af66b552c9af
Mike <mike@graywind.org>
2023-08-21 21:11:55 -0500

    fix: modernize and clean (#16)
    Co-authored-by: JarbasAI <33701864+JarbasAl@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -9,27 +9,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from os.path import join, dirname
+from os.path import dirname, join
 
-from adapt.intent import IntentBuilder
-from mycroft.skills.core import intent_handler
 from neon_solver_wikipedia_plugin import WikipediaSolver
 from ovos_utils import classproperty
+from ovos_utils.intents import IntentBuilder
 from ovos_utils.gui import can_use_gui
 from ovos_utils.process_utils import RuntimeRequirements
+from ovos_workshop.decorators import intent_handler
 from ovos_workshop.skills.common_query_skill import CommonQuerySkill, CQSMatchLevel
 
 
 class WikipediaSkill(CommonQuerySkill):
-    def __init__(self):
-        super(WikipediaSkill, self).__init__(name="WikipediaSkill")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def initialize(self):
         if "lang" in self.settings:
             lang = self.settings["lang"]
         else:
             lang = self.lang.split("-")[0]
-
         self.wiki = WikipediaSolver(config={"lang": lang})
 
         # for usage in tell me more / follow up questions
@@ -39,24 +37,25 @@ class WikipediaSkill(CommonQuerySkill):
 
     @classproperty
     def runtime_requirements(self):
-        return RuntimeRequirements(internet_before_load=True,
-                                   network_before_load=True,
-                                   gui_before_load=False,
-                                   requires_internet=True,
-                                   requires_network=True,
-                                   requires_gui=False,
-                                   no_internet_fallback=False,
-                                   no_network_fallback=False,
-                                   no_gui_fallback=True)
+        return RuntimeRequirements(
+            internet_before_load=True,
+            network_before_load=True,
+            gui_before_load=False,
+            requires_internet=True,
+            requires_network=True,
+            requires_gui=False,
+            no_internet_fallback=False,
+            no_network_fallback=False,
+            no_gui_fallback=True,
+        )
 
     # intents
     @intent_handler("wiki.intent")
     def handle_search(self, message):
-        """ Extract what the user asked about and reply with info
-            from wikipedia.
+        """Extract what the user asked about and reply with info
+        from wikipedia.
         """
-        self.gui.show_animated_image(join(dirname(__file__), "ui",
-                                          "jumping.gif"))
+        self.gui.show_animated_image(join(dirname(__file__), "ui", "jumping.gif"))
         self.current_title = query = message.data["query"]
         self.speak_dialog("searching", {"query": query})
         self.image = None
@@ -68,37 +67,36 @@ class WikipediaSkill(CommonQuerySkill):
 
     # @intent_handler("wikiroulette.intent")
     def handle_wiki_roulette_query(self, message):
-        """ Random wikipedia page """
-        self.gui.show_animated_image(join(dirname(__file__), "ui",
-                                          "jumping.gif"))
+        """Random wikipedia page"""
+        self.gui.show_animated_image(join(dirname(__file__), "ui", "jumping.gif"))
         self.image = None
         self.current_title = "Wiki Roulette"
         self.speak_dialog("wikiroulette")
         # TODO
 
-    @intent_handler(IntentBuilder("WikiMore").require("More").
-                    require("wiki_article"))
+    @intent_handler(IntentBuilder("WikiMore").require("More").require("wiki_article"))
     def handle_tell_more(self, message):
-        """ Follow up query handler, "tell me more".
+        """Follow up query handler, "tell me more".
 
-            If a "spoken_lines" entry exists in the active contexts
-            this can be triggered.
+        If a "spoken_lines" entry exists in the active contexts
+        this can be triggered.
         """
         self.speak_result()
 
     # common query
-    def CQS_match_query_phrase(self, utt):
-        title, summary = self.ask_the_wiki(utt)
+    def CQS_match_query_phrase(self, phrase):
+        title, summary = self.ask_the_wiki(phrase)
         if summary:
             self.idx += 1  # spoken by common query
-            return (utt, CQSMatchLevel.GENERAL, summary,
-                    {'query': utt,
-                     'image': self.image,
-                     'title': title,
-                     'answer': summary})
+            return (
+                phrase,
+                CQSMatchLevel.GENERAL,
+                summary,
+                {"query": phrase, "image": self.image, "title": title, "answer": summary},
+            )
 
     def CQS_action(self, phrase, data):
-        """ If selected show gui """
+        """If selected show gui"""
         self.display_wiki_entry()
         self.set_context("WikiKnows", data.get("title") or phrase)
 
@@ -109,7 +107,8 @@ class WikipediaSkill(CommonQuerySkill):
         self.idx = 0
         try:
             self.results = self.wiki.long_answer(query, lang=self.lang)
-        except:  # handle solver plugin failures, happens in some queries
+        except Exception as err:  # handle solver plugin failures, happens in some queries
+            self.log.error(err)
             self.results = None
 
         self.image = self.wiki.get_image(query)
@@ -123,9 +122,7 @@ class WikipediaSkill(CommonQuerySkill):
             return
         image = image or self.image
         if image:
-            self.gui.show_image(image,
-                                title=title, fill=None,
-                                override_idle=20, override_animations=True)
+            self.gui.show_image(image, title=title, fill=None, override_idle=20, override_animations=True)
 
     def speak_result(self):
         if self.idx + 1 > len(self.results):
@@ -140,7 +137,3 @@ class WikipediaSkill(CommonQuerySkill):
 
     def stop(self):
         self.gui.release()
-
-
-def create_skill():
-    return WikipediaSkill()
