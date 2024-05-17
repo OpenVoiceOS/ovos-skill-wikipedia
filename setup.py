fad2dadbd0b3889c4d2e3ca3abe98ece5fec14a7
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2024-05-17 10:38:53 -0700

    Update setup.py and GHA to resolve PyPI errors (#41)
    Resolves version bump error https://github.com/OpenVoiceOS/skill-ovos-wikipedia/actions/runs/9121712937/job/25081332652
    Closes #30
    Update setup.py to use absolute file paths
    
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

--- setup.py
+++ setup.py
@@ -15,11 +15,10 @@ PLUGIN_ENTRY_POINT = f'{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}={SKILL_PKG}:{
 # skill_id=package_name:SkillClass
 
 SOLVER_ENTRY_POINT = f'ovos-solver-plugin-wikipedia={SKILL_PKG}:WikipediaSolver'
-
+BASE_DIR = path.abspath(path.dirname(__file__))
 
 def get_requirements(requirements_filename: str):
-    requirements_file = path.join(path.abspath(path.dirname(__file__)),
-                                  requirements_filename)
+    requirements_file = path.join(BASE_DIR, requirements_filename)
     with open(requirements_file, 'r', encoding='utf-8') as r:
         requirements = r.readlines()
     requirements = [r.strip() for r in requirements if r.strip()
@@ -44,10 +43,11 @@ def find_resource_files():
     return package_data
 
 
-with open("README.md", "r") as f:
+with open(path.join(BASE_DIR, "README.md"), "r") as f:
     long_description = f.read()
 
-with open("./version.py", "r", encoding="utf-8") as v:
+
+with open(path.join(BASE_DIR, "version.py"), "r", encoding="utf-8") as v:
     for line in v.readlines():
         if line.startswith("__version__"):
             if '"' in line:
@@ -58,7 +58,8 @@ with open("./version.py", "r", encoding="utf-8") as v:
 setup(
     name=PYPI_NAME,
     version=version,
-    description='ovos wikipedia skill plugin', long_description=long_description,
+    description='ovos wikipedia skill plugin',
+    long_description=long_description,
     url=URL,
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
