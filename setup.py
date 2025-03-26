9c1c40ada7ef27c8ba098b70023740d3818b0ae2
miro <jarbasai@mailfence.com>
2025-03-26 21:15:34 +0000

    refactor:split plugin into own repo

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
     version=get_version(),
     description='ovos wikipedia skill plugin',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url=URL,
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
